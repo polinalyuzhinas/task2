8b8f3dc933a7614da5d134c5d6497aa74a07b17b
polinalyuzhinas <polinalyuzhinas@yandex.ru>
2024-04-08 20:42:14 +0300

    Added fib(n) function

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-cout << "Hello world!\n";
+int fib(int n) {
+        if (n <= 0) {
+                return -1;
+        }
+        else if (n == 1 || n == 2) {
+                return 1;
+        }
+        else {
+                return fib(n-2) + fib(n-1);
+        }
+}
+
+int main()
+{
+    int n;
+    cout << "Hello world!\n";
+    cout << "Input the number of the member of the Fibonacci sequence you want to know: "; cin >> n;
+    cout << n << "th member of the Fibonacci sequence - " << fib(n) << endl;
 }
 
